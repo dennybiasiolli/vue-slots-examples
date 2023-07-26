34a99ee85026d5914f050387d58eb5228d53ebaa
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-07-26 11:25:57 +0200

    adding FiltersView samples

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/filters',
+      name: 'filters',
+      // route level code-splitting
+      // this generates a separate chunk (Filters.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/FiltersView.vue')
     }
   ]
 })
---
+++ src/views/FiltersView.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { customFilter } from '@/utils/filters'
+
+const array = [1, '', 'a', null, 'hello', undefined, 42, 0, 10, false, '0', true]
+console.log(array)
+console.log(JSON.stringify(array))
+
+function getElementString(element: any) {
+  return JSON.stringify(element) || 'undefined'
+}
+</script>
+
+<template>
+  <div>
+    <h1>Filters page</h1>
+
+    <h2>Original array</h2>
+    <h3>
+      [
+      <span v-for="element in array" :key="getElementString(element)">
+        {{ getElementString(element) }},
+      </span>
+      ]
+    </h3>
+
+    <h2>Filter 1: remove `undefined` elements</h2>
+    <h3>{{ customFilter(array) }}</h3>
+
+    <h2>Filter 2: optionally remove `null` elements</h2>
+    <h3>{{ customFilter(array, true, true) }}</h3>
+
+    <h2>Filter 3: optionally remove `false` elements</h2>
+    <h3>{{ customFilter(array, true, true, true) }}</h3>
+
+    <h2>Filter 4: optionally remove `''` elements</h2>
+    <h3>{{ customFilter(array, true, true, true, true) }}</h3>
+
+    <h2>Filter 5: optionally remove `0` elements</h2>
+    <h3>{{ customFilter(array, true, true, true, true, true) }}</h3>
+
+    <h2>Filter 6: optionally remove `'0'` elements</h2>
+    <h3>{{ customFilter(array, true, true, true, true, true, true) }}</h3>
+  </div>
+</template>
+
+<style scoped>
+h1,
+h2,
+h3 {
+  color: var(--color-heading);
+  font-weight: bold;
+}
+
+h3 {
+  padding-bottom: 1.5rem;
+}
+</style>
