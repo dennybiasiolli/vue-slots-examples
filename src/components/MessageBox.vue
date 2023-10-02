0a43f6173b750fe8d409bf310932de87e0d55f0f
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-10-02 12:41:48 +0200

    adding MessageBox example

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+export interface Props {
+  title?: string
+  message?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div v-if="title || $slots.title" class="message-box-title">
+    <slot name="title">
+      {{ title }}
+    </slot>
+  </div>
+
+  <div class="message-box-message">
+    <slot>
+      {{ message }}
+    </slot>
+  </div>
+
+  <div v-if="$slots.footer" class="message-box-footer">
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<style scoped>
+.message-box-title {
+  background-color: darkslategray;
+  font-size: large;
+  padding: 5px;
+}
+
+.message-box-message {
+  background-color: slategray;
+  font-size: large;
+  padding: 10px;
+}
+
+.message-box-footer {
+  background-color: gray;
+  font-size: large;
+  padding: 5px 10px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,14 @@ const router = createRouter({
       component: () => import('../views/FiltersView.vue')
     },
     {
+      path: '/message-box',
+      name: 'message-box',
+      // route level code-splitting
+      // this generates a separate chunk (MessageBoxView.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MessageBoxView.vue')
+    },
+    {
       path: '/slots-examples',
       name: 'slots-examples',
       // route level code-splitting
